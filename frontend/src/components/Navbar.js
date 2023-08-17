5f3c09ea4e4dc7881321587fc4e52da52a496fb8
Klymenko Illia <sovuh2703@gmail.com>
2023-08-17 19:30:49 +0300

    upgrade responsive navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,36 +7,40 @@ const Navbar = () => {
 
 
     return (
-        <nav className={base_styles.navbar}>
-            <div className={base_styles.left}>
-                <div className={base_styles.logo}><img src={logo} alt=""/></div>
-                <div className={base_styles.link_wrapper}>
-                    <ul className={base_styles["nav-links"]}>
-                        <li><a className={base_styles["nav-link"]} href="#">Home</a></li>
-                        <li><a className={base_styles["nav-link"]} href="#">TopList</a></li>
-                        <li><a className={base_styles["nav-link"]} href="#">About</a></li>
-                        <li><a className={base_styles["nav-link"]} href="#">Log In</a></li>
-                    </ul>
+        <>
+            <nav className={base_styles.navbar}>
+                <div className={base_styles.left}>
+                    <div className={base_styles.logo}><img src={logo} alt=""/></div>
+                    <div className={base_styles.link_wrapper}>
+                        <ul className={base_styles["nav-links"]}>
+                            <li><a className={base_styles["nav-link"]} href="#">Home</a></li>
+                            <li><a className={base_styles["nav-link"]} href="#">TopList</a></li>
+                            <li><a className={base_styles["nav-link"]} href="#">About</a></li>
+                            <li><a className={base_styles["nav-link"]} href="#">Log In</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            {isShown && <nav className={base_styles["burger-nav"]} style={{color: "#fff"}}>
-                <a className={base_styles["nav-link"]} href="#">Home</a>
-                <a className={base_styles["nav-link"]} href="#">TopList</a>
-                <a className={base_styles["nav-link"]} href="#">About</a>
-                <a className={base_styles["nav-link"]} href="#">Log In</a>
-                <a className={base_styles["play-button"]} href="#">Play</a>
-            </nav>}
-            <div className={base_styles.right}>
-                <a className={base_styles["play-button"]} href="#">Play</a>
-            </div>
-            <div className={base_styles.burger}>
+                <div className={base_styles.right}>
+                    <a className={base_styles["play-button"]} href="#">Play</a>
+                </div>
+                <div className={base_styles.burger}>
                 <span onClick={() => setIsShown(!isShown)}>
                     <ion-icon name="reorder-three-outline"></ion-icon>
                 </span>
 
-            </div>
+                </div>
 
-        </nav>
+            </nav>
+            {
+                isShown && <nav className={base_styles["burger-nav"]} style={{color: "#fff"}}>
+                    <a className={base_styles["nav-link"]} href="#">Home</a>
+                    <a className={base_styles["nav-link"]} href="#">TopList</a>
+                    <a className={base_styles["nav-link"]} href="#">About</a>
+                    <a className={base_styles["nav-link"]} href="#">Log In</a>
+                    <a className={base_styles["play-button"]} href="#">Play</a>
+                </nav>
+            }
+        </>
     )
 }
 
