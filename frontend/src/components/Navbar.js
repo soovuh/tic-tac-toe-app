91feb6bbf9713ee16089f5d9fba7d2163a13480c
Klymenko Illia <sovuh2703@gmail.com>
2023-08-17 18:39:57 +0300

    Create responsive for navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import base_styles from '../styles/base.module.css'
 import logo from '../logo.png'
 
-const Navbar = () => (
-    <nav className={base_styles.navbar}>
-        <div className={base_styles.left}>
-            <div className={base_styles.logo}><img src={logo} alt=""/></div>
-            <div className={base_styles.link_wrapper}>
-                <ul className={base_styles["nav-links"]}>
-                    <li><a className={base_styles["nav-link"]} href="#">Home</a></li>
-                    <li><a className={base_styles["nav-link"]} href="#">Toplist</a></li>
-                    <li><a className={base_styles["nav-link"]} href="#">About</a></li>
-                    <li><a className={base_styles["nav-link"]} href="#">Log In</a></li>
-                </ul>
-            </div>
-        </div>
-        <div className={base_styles.right}>
-            <a href="#">Play</a>
-        </div>
+const Navbar = () => {
+    const [isShown, setIsShown] = useState(false)
 
-    </nav>
-)
+
+    return (
+        <nav className={base_styles.navbar}>
+            <div className={base_styles.left}>
+                <div className={base_styles.logo}><img src={logo} alt=""/></div>
+                <div className={base_styles.link_wrapper}>
+                    <ul className={base_styles["nav-links"]}>
+                        <li><a className={base_styles["nav-link"]} href="#">Home</a></li>
+                        <li><a className={base_styles["nav-link"]} href="#">TopList</a></li>
+                        <li><a className={base_styles["nav-link"]} href="#">About</a></li>
+                        <li><a className={base_styles["nav-link"]} href="#">Log In</a></li>
+                    </ul>
+                </div>
+            </div>
+            {isShown && <nav className={base_styles["burger-nav"]} style={{color: "#fff"}}>
+                <a className={base_styles["nav-link"]} href="#">Home</a>
+                <a className={base_styles["nav-link"]} href="#">TopList</a>
+                <a className={base_styles["nav-link"]} href="#">About</a>
+                <a className={base_styles["nav-link"]} href="#">Log In</a>
+                <a className={base_styles["play-button"]} href="#">Play</a>
+            </nav>}
+            <div className={base_styles.right}>
+                <a className={base_styles["play-button"]} href="#">Play</a>
+            </div>
+            <div className={base_styles.burger}>
+                <span onClick={() => setIsShown(!isShown)}>
+                    <ion-icon name="reorder-three-outline"></ion-icon>
+                </span>
+
+            </div>
+
+        </nav>
+    )
+}
 
 export default Navbar;
