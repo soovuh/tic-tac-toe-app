d23f8d66aff68bba0c126442574260670dd69c4b
Klymenko Illia <sovuh2703@gmail.com>
2023-08-29 15:53:02 +0300

    rework home page when user isAuthenticated

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -2,16 +2,39 @@ import React from "react";
 import base_styles from '../styles/base.module.css'
 import styles from '../styles/home.module.css'
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 
-const Home = () => (
-    <div className={base_styles.wrapper}>
-        <div className={styles.greetings_wrapper}>
-            <h1 className={styles.header}>Welcome to Tic-Tac-Toe online!</h1>
-            <p className={styles.intro}>This is online version of tic-tac-toe game</p>
-            <p className={styles.click_info}>Click the login button for start</p>
-            <Link className={styles.login_btn} to='/login'>Log In</Link>
+
+const Home = ({isAuthenticated}) => {
+    if (isAuthenticated) {
+        return (
+            <div className={base_styles.wrapper}>
+                <div className={styles.greetings_wrapper}>
+                    <h1 className={styles.header}>Welcome back!</h1>
+                    <p className={styles.intro}>You're back to the online Tic-Tac-Toe game!</p>
+                    <p className={styles.click_info}>Click the play button to start a new game</p>
+                    <Link className={styles.play_btn} to='/lobby'>Play Now</Link>
+                </div>
+            </div>
+
+        )
+    }
+    return (
+        <div className={base_styles.wrapper}>
+            <div className={styles.greetings_wrapper}>
+                <h1 className={styles.header}>Welcome to Tic-Tac-Toe online!</h1>
+                <p className={styles.intro}>This is online version of tic-tac-toe game</p>
+                <p className={styles.click_info}>Click the login button for start</p>
+                <Link className={styles.login_btn} to='/login'>Log In</Link>
+            </div>
         </div>
-    </div>
-)
+    )
 
-export default Home;
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+
+export default connect(mapStateToProps, {})(Home);
