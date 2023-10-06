28b641464c5acec463cb463425621f10075d23cf
Klymenko Illia <sovuh2703@gmail.com>
2023-10-06 17:15:59 +0300

    change asgi file

--- game_api/asgi.py
+++ game_api/asgi.py
@@ -1,13 +1,18 @@
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from game_api import routing
 
+asgi_app = get_asgi_application()
+
+from game_api import routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "game_api.settings")
+django.setup()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
--- game_api/routing.py
+++ game_api/routing.py
@@ -1,4 +1,3 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 from core.consumers.search import SearchConsumer
 from core.consumers.game import GameConsumer
