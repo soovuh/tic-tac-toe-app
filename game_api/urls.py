03ff2f20497141415c71f09dc270cae96f26a42f
Klymenko Illia <sovuh2703@gmail.com>
2023-10-06 17:26:28 +0300

    change staticfiles patterns

--- game_api/urls.py
+++ game_api/urls.py
@@ -7,7 +7,9 @@ from rest_framework.routers import DefaultRouter
 from accounts.views import TopListViewSet
 from core.views import GameAPIView
 from game_api import routing
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.views.static import serve
+
 
 router = DefaultRouter()
 router.register(r'top-list', TopListViewSet)
@@ -18,9 +20,11 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
     path('ws/', include(routing.websocket_urlpatterns)),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 
 urlpatterns += [re_path(r'^,*', TemplateView.as_view(template_name='index.html'))]
-urlpatterns += staticfiles_urlpatterns()
+
